ab6be0509749cd2b28da1b505549f80ccda7a0b0
Manoel Tindao <timdao02@hotmail.com>
2020-10-06 15:54:27 -0300

    Criação do projeto

---
+++ src/main/java/com/manoel/algamoneyapi02/AlgamoneyApi02Application.java
@@ -0,0 +1,13 @@
+package com.manoel.algamoneyapi02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgamoneyApi02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgamoneyApi02Application.class, args);
+	}
+
+}
